e6ce4bdffb4843c252a7e016b753801215db8d93
nail160306 <nail160306@outlook.com>
2018-06-09 11:44:22 +0300

    Продолжение

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -14,9 +14,11 @@ struct question
 };
 
 vector<question> get_questions();
-void read_quests(vector<question>&, ifstream&);
+void read_quests(vector<question>&, ifstream& const);
+void read_answers(question& que, ifstream& const ifile);
 int interview(vector<question>);
 void out_intformation(int);
+
 
 main()
 {
@@ -25,28 +27,73 @@ main()
 		vector<question> questions = get_questions();
 		int count_right = interview(questions);
 		out_intformation(count_right);
+		keep_window_open();
 	}
 	catch (const std::exception& e)
 	{
-		cout << e.what
+		cout << e.what << '\n';
+		keep_window_open();
 	}
 	SetConsoleOutputCP(1251);
 
 	
 }
 
+//Получает вопросы
 vector<question> get_questions()
 {
 	vector<question> questions;
 	ifstream ifile{ "questions.txt" };
 	read_quests(questions, ifile);
 	ifile.close();
+	return questions;
 }
 
-void read_quests(vector<question>& questions, ifstream& ifile)
+//Читает вопросы из файла
+void read_quests(vector<question>& questions, ifstream& const ifile)
 {
 	while (!ifile.eof())
 	{
+		char mark;
+		ifile >> mark;
+		if (mark != '?')
+			error("ОШИБКА: вопрос не найден");
+		
+		question que;
+		ifile >> que.quest;
+		read_answers(que, ifile);
+		questions.push_back(que);
+	}
+}
 
+//Читает ответы из файла
+void read_answers(question& que, ifstream& const ifile)
+{
+	char mark{' '};
+	while (mark != '?')
+	{
+		ifile >> mark;
+		if (mark == '\n')
+			continue;
+
+		if (mark != '+' && mark != '-')
+			error("ОШИБКА: не найдено ответа");
+
+		answer ans;
+		ifile >> ans.ans;
+		if (mark == '+')
+			ans.right = true;
+		else
+			ans.right = false;
+
+		que.answers.push_back(ans);
+	}
+}
+
+int interview(vector<question> quests)
+{
+	for (int i = 0; i != quests.size(); i++)
+	{
+		
 	}
 }
