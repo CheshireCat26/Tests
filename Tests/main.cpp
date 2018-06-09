49ebc2a382c0e366d51ca3703848f0a9eef01c06
nail160306 <nail160306@outlook.com>
2018-06-09 12:05:37 +0300

    Продолжение

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -17,7 +17,9 @@ vector<question> get_questions();
 void read_quests(vector<question>&, ifstream& const);
 void read_answers(question& que, ifstream& const ifile);
 int interview(vector<question>);
-void out_intformation(int);
+void out_quest(question const que);
+vector<int> get_user_ans();
+void out_intformation(int, int);
 
 
 main()
@@ -26,7 +28,7 @@ main()
 	{
 		vector<question> questions = get_questions();
 		int count_right = interview(questions);
-		out_intformation(count_right);
+		out_intformation(count_right, questions.size());
 		keep_window_open();
 	}
 	catch (const std::exception& e)
@@ -90,9 +92,43 @@ void read_answers(question& que, ifstream& const ifile)
 	}
 }
 
-int interview(vector<question> quests)
+int interview(vector<question> const quests)
 {
+	int sum_rigth;
 	for (int i = 0; i != quests.size(); i++)
+	{
+		out_quest(quests[i]);
+		cout << "> ";
+		vector<int> user_ans = get_user_ans();
+		if (check_answer(quests[i], user_ans))
+			sum_rigth++;
+	}
+}
+
+void out_quest(question const que)
+{
+	cout << que.quest << '\n';
+	const vector<answer>& this_answers = que.answers;
+	for (int i = 0; i != this_answers.size(); i++)
+	{
+		const answer& ans = this_answers[i];
+		cout << '\t' << ans.ans << '\n';
+	}
+}
+
+vector<int> get_user_ans()
+{
+	vector<int> user_ans;
+	for (int i; cin >> i;)
+		user_ans.push_back(i);
+	return user_ans;
+}
+
+bool check_answer(const question& que, const int sum_rigth)
+{
+	bool ret{true};
+
+	for (int i = 0; i != que.answers.size(); i++)
 	{
 		
 	}
